2ad398f77b0cc733a4d10a4f0be89938a51644cd
mini2929 <haloyun29@gmail.com>
2024-10-24 20:59:47 +0900

    스크롤 모션 기능 부여하였으나 정지된 상태, 고치기

--- src/components/main/Pics.jsx
+++ src/components/main/Pics.jsx
@@ -1,9 +1,15 @@
-export default function Pics({Scrolled}) {
-	console.log(Scrolled);
+export default function Pics({scrolled,pos}) {
+	console.log(pos);
 	return (
 		<section className="pics">
+			{/* pos ||0의 의미는 초기에 pos값이 undefined이면 일단은 0으로 대신 값을 집어넣음 */}
+			{/* pos가 usRef로 참조된 값이므로 첫번째 렌더링 시에 pos값이 undefined로 나오면서 - 연산에서 발생 : 해당 에러를 스킵하기 위함 */}
+			<h2 style={{left: scrolled - pos || 0}}>TYPOGRAPHY</h2>
 			<div className="box"
-			style ={{transform: `rotate(${Scrolled/10}deg)` }}></div>
+			style ={{transform: `rotate(${scrolled - pos}deg)` }}></div>
 		</section>
 	)
 }
+
+
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,41 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import Banner from '../components/main/Banner';
 import Pics from '../components/main/Pics';
 import Vids from '../components/main/Vids';
 import Visual from '../components/main/Visual';
 
 export default function Home() {
-	const [Scrolled, setScrolled] = useState(0);
+	const [scrolled, setScrolled] = useState(0);
+// 객체함수
+	const ref_el = useRef(null);
+	const ref_posArr = useRef([]);
+	
+// 브라우저 리사이즈 시 해당 메인 프레임으 ㅣ자식 요소를 반복돌면서 각 요소의 세로 위치값을 ref_posArr에 담아주는 함수 
+	const getPos = () => {
+		ref_posArr.current = [];
+		for (const el of ref_el.current.children) ref_posArr.current.push(el.offsetTop);
+	};
+
 	const handleScroll = () => {
 		setScrolled(window.scrollY);
 	}
 
 	useEffect(()=> {
+		getPos();
+		window.addEventListener('resize', getPos);
 		window.addEventListener('scroll', handleScroll);
-		return () => window.removeEventListener('scroll', handleScroll);
+		return () => {
+			window.removeEventListener('resize', getPos);
+			window.removeEventListener('scroll', handleScroll);}
 		}, []);
 
 	return (
-		<main>
+		<main ref={ref_el}>
 			<Visual />
-			<Pics Scrolled={Scrolled}/>
+{/* 해당 Home 컴포넌트에서 만들어지는 자식 섹션들의 세로 위치 배열값 중 2번째에 해당하는 Pics의 위치값만 pos라는 props로 전달 */}
+{/* pos값이 필요한 이유 해당 컴포넌트는 문서 상단에 위치해 있지 않기 때문에 현재 스크롤되고 있는 거리값에서 해당 섹션의 위치값을 빼줘야지 0으로 초기화된 값으로 스타일 적용 가능 */}
+
+			<Pics Scrolled={scrolled} pos = {ref_posArr.current[1]}/>
 			<Vids />
 			<Banner />
 		</main>
